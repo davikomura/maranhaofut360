201127cc03c349a1fd4f26e1cb38c3f2e413de70
Davi Komura de Castro <davi.komura@gmail.com>
2025-11-26 14:22:34 -0300

    updates site

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,11 @@ import './index.css'
 import './i18n';
 import {RouterProvider} from 'react-router-dom'
 import router from './routes.jsx'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <RouterProvider router={router} />
   </StrictMode>,
 )
